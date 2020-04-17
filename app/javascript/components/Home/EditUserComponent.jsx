8199447d2db932c12bfeab9245566319b1a6ecda
Manthan <chougulemanthan2@gmail.com>
2020-04-17 19:52:49 +0530

    update form

--- app/javascript/components/Home/EditUserComponent.jsx
+++ app/javascript/components/Home/EditUserComponent.jsx
@@ -1,6 +1,5 @@
 import React,{ Component } from 'react'
-import Button from '@material-ui/core/Button'
-import { Formik, Form, ErrorMessage } from 'formik'
+import { Formik, Form, ErrorMessage, Field } from 'formik'
 import { FormSchema } from '../common/validation'
 
 
@@ -20,37 +19,40 @@ class EditUserComponent extends Component {
         validationSchema={FormSchema}
         onSubmit={(values,{ setSubmitting }) => {
           setTimeout(()=>{
-            () => this.props.updateUser(values);
+            console.log("Clicked");
+            this.props.updateUser(values)
             console.log("Formik ReSubmit", values);
             console.log("RESubmited");
             setSubmitting(false);
           },400)
         }}
+
       >
         <Form>
         <div>
           <label htmlFor="newFirstname">New First Name </label> <br />
-          <input name="newFirstname" type="text" defaultValue={this.props.user.firstname}/> <br />
+          <Field name="newFirstname" type="text" placeholder={this.props.user.firstname}/> <br />
           <ErrorMessage name="firstname" /> <br />
         </div>
         <div>
           <label htmlFor="newLastname">New Last Name </label> <br />
-          <input name="newLastname" type="text" defaultValue={this.props.user.lastname}/> <br />
+          <Field name="newLastname" type="text" placeholder={this.props.user.lastname}/> <br />
           <ErrorMessage name="lastname" /> <br />
         </div>
         <div>
-          <label htmlFor="newContact">Contact </label> <br />
-          <input name="newContact" type="text" defaultValue={this.props.user.contact} /> <br />
+          <label htmlFor="newContact">New Contact </label> <br />
+          <Field name="newContact" type="text" placeholder={this.props.user.contact} /> <br />
           <ErrorMessage name="contact" /> <br />
         </div>
         <div>
-          <label htmlFor="newAddress">Address</label> <br />
-          <input name="newAddress" type="text" defaultValue={this.props.user.address}/> <br />
+          <label htmlFor="newAddress">New Address</label> <br />
+          <Field name="newAddress" type="text" placeholder={this.props.user.address}/> <br />
           <ErrorMessage name="address" /> <br />
         </div>
-          <button type="submit">
-            Update
-          </button>
+        <button type="submit"
+        //onClick={() => this.props.updateUser(initialValues)}
+        >
+          Submit</button>
         </Form>
         </Formik>
       </div>
